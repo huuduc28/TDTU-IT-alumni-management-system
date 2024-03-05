5d4403944a22893c1c186a24cc5abeb38eb265f2
Phạm Nguyễn <113409512+JumpToTheSky@users.noreply.github.com>
2024-03-06 00:56:05 +0700

    loginController fixed

--- TDTU-IT-alumni-management-system/Controllers/LoginController.cs
+++ TDTU-IT-alumni-management-system/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TDTU_IT_alumni_management_system.Models; // Thêm namespace này
 
 namespace TDTU_IT_alumni_management_system.Controllers
 {
@@ -12,6 +13,32 @@ namespace TDTU_IT_alumni_management_system.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        // POST: Login/Login 
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+            
+            using (var db = new TDTUAlumnisManagementSystemEntities())
+            {
+                
+                var user = db.Users.Where(u => u.UsersName == Username && u.Password == Password).FirstOrDefault();
+
+                if (user != null)
+                {
+                    
+                    Session["Username"] = user.UsersName;
+
+                    
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    
+                    return Json(new { success = false, message = "Tên đăng nhập hoặc mật khẩu không hợp lệ!" });
+                }
+            }
         }
     }
 }
