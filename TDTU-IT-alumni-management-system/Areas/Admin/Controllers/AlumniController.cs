9e017b8de5b316b9d54a9269f512e82e266629a5
huuduc28 <117356967+huuduc28@users.noreply.github.com>
2024-03-13 02:56:45 +0700

    Complete pagination, search and filtering of alumni information

--- TDTU-IT-alumni-management-system/Areas/Admin/Controllers/AlumniController.cs
+++ TDTU-IT-alumni-management-system/Areas/Admin/Controllers/AlumniController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PagedList;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -15,11 +17,14 @@ namespace TDTU_IT_alumni_management_system.Areas.Admin.Controllers
         private TDTUAlumnisManagementSystemEntities db = new TDTUAlumnisManagementSystemEntities();
 
         // GET: Admin/Alumni
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View(db.Alumni.ToList());
+            var pageSize = 10;
+            var pageNumber = (page ?? 1); 
+            var data = db.Alumni.Include(r => r.GraduationInfo).ToList();
+            var pagedData = data.ToPagedList(pageNumber, pageSize);
+            return View(pagedData);
         }
-
         // GET: Admin/Alumni/Details/5
         public ActionResult Details(string id)
         {
@@ -122,6 +127,31 @@ namespace TDTU_IT_alumni_management_system.Areas.Admin.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        public ActionResult Search(string searchString, int? page)
+        {
+            var pageSize = 10; 
+            var pageNumber = (page ?? 1); 
+            var data = db.Alumni.OrderByDescending(n => n.datebegin).ToList();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                data = data.Where(n => n.IDAlumni.Contains(searchString) || n.Name.Contains(searchString)).ToList();
+            }
+            var pagedData = data.ToPagedList(pageNumber, pageSize);
+            return View("Index", pagedData);
+        }
+        public ActionResult DataFilter(string majorKey)
+        {
+       
+            var query = db.Alumni.Include(r => r.GraduationInfo);
+            // Lọc theo ngành học nếu có
+            if (!string.IsNullOrEmpty(majorKey))
+            {
+                query = query.Where(a => a.GraduationInfo.Majors == majorKey);
+            }
+            var filteredData = query.ToList();
+            return PartialView("_AlumniPartialView", filteredData);
         }
     }
 }
