f7a7c54bb479f57df5aa4a89dedd1cc5a5d9d5d4
Phạm Nguyễn <113409512+JumpToTheSky@users.noreply.github.com>
2024-03-20 19:29:55 +0700

    Send email notification about the latest news

--- TDTU-IT-alumni-management-system/Areas/Admin/Controllers/NotifiesController.cs
+++ TDTU-IT-alumni-management-system/Areas/Admin/Controllers/NotifiesController.cs
@@ -298,8 +298,8 @@ namespace TDTU_IT_alumni_management_system.Areas.Admin.Controllers
             {
                 message.To.Add(new MailboxAddress(item.Email, item.Email));
             }
-            message.Subject = "Thông báo mới từ TDTU Alumnis:";
-            message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = notify.Title + "<br>" + "<br>" + notify.Description + "<br>" +
+            message.Subject = "Thông báo mới từ TDTU Alumnis:" + notify.Title;
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text =  notify.Description + "<br>" + notify.Content +
                             "<br><br><a href=\"" + notifyLink + "\"><button style=\"background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer;\">Link thông báo</button></a>" };
 
             // Tạo SMTP client
